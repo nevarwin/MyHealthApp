49d8b2b71122b0509bf2d5c2c533f1d04d61f0f3
nevarwin <ravencsolis@gmail.com>
2025-11-22 20:31:02 +0800

    Initial Commit

---
+++ MyHealthApp/MyHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthAppApp.swift
+//  MyHealthApp
+//
+//  Created by raven on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyHealthAppTests/MyHealthAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthAppTests.swift
+//  MyHealthAppTests
+//
+//  Created by raven on 11/22/25.
+//
+
+import Testing
+@testable import MyHealthApp
+
+struct MyHealthAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
